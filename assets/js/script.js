71d010b0510ecc06e9792f2a3ad98b897ca7b49d
JoshuaAndrews-1882 <joshuathfc@icloud.com>
2022-05-28 12:39:43 +0000

    Add JS and CSS code to show hidden compare div and hide game-area div when user choice is selected

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,4 +3,6 @@ const userChoice = (choice) => {
     console.log(choice);
     let choices = document.querySelector(".game-area");
     choices.style.display = "none";
-}
+    let compare = document.querySelector(".compare");
+    compare.style.visibility = "visible";
+};
