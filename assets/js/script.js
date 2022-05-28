5fd2068900a2414633ede1f09f2cea1f165f8b2c
JoshuaAndrews-1882 <joshuathfc@icloud.com>
2022-05-28 14:17:55 +0000

    Add JS code to change image to what the player selects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,27 @@
+/*
+const gameChoices = {
+    "rock": "/assets/images/rock.png",
+    "paper": "/assets/images/paper.png",
+    "scissors": "/assets/images/paper.png"
+}
+*/
+
 
 const userChoice = (choice) => {
-    console.log(choice);
     let choices = document.querySelector(".game-area");
     choices.style.display = "none";
     let compare = document.querySelector(".compare");
     compare.style.visibility = "visible";
-};
+
+    switch (choice) {
+        case 'rock':
+            document.getElementById("game-choice").src = "/assets/images/rock.png";
+            break;
+        case 'paper':
+            document.getElementById("game-choice").src = "/assets/images/paper.png";
+            break;
+        case 'scissors':
+            document.getElementById("game-choice").src = "/assets/images/scissors.png";
+            break;
+}
+}
